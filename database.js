3977722123cb9333eb81e33b2f601f348d164010
pavanleton <pavankumarkpkp@gmail.com>
2021-08-13 20:35:40 +0530

    add temp database

---
+++ database.js
@@ -0,0 +1,50 @@
+// Temporary Database
+
+let Book = [
+  {
+    ISBN: "12345ONE",
+    title: "Getting started with MERN",
+    authors: [1, 2],
+    language: "en",
+    pubDate: "2021-07-07",
+    numOfPage: 225,
+    category: ["fiction", "programming", "tech", "web dev"],
+    publication: 1,
+  },
+  {
+    ISBN: "12345Two",
+    title: "Getting started with Python",
+    authors: [1, 2],
+    language: "en",
+    pubDate: "2021-07-07",
+    numOfPage: 225,
+    category: ["fiction", "tech", "web dev"],
+    publication: 1,
+  },
+];
+
+let Author = [
+  {
+    id: 1,
+    name: "pavan",
+    books: ["12345ONE", "12345Two"],
+  },
+  {
+    id: 2,
+    name: "Deepak",
+    books: ["12345ONE"],
+  },
+];
+
+let Publication = [
+  {
+    id: 1,
+    name: "Chakra",
+    books: ["12345ONE"],
+  },
+  {
+    id: 2,
+    name: "Vickie Publications",
+    books: [],
+  },
+];
