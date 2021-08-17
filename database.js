334973cc53dd43af13401e0e36519bc71ebb9848
aditya12gusain <adisandi3112002@gmail.com>
2021-08-17 19:40:02 +0530

    Post Route Creation for Book, Author, Publication Addition
    With static data in a js file

--- database.js
+++ database.js
@@ -1,52 +1,52 @@
 // Temporary Database
-
+// Array of objects
 let Book = [
-  {
-    ISBN: "12345ONE",
-    title: "Getting started with MERN",
-    authors: [1, 2],
-    language: "en",
-    pubDate: "2021-07-07",
-    numOfPage: 225,
-    category: ["fiction", "programming", "tech", "web dev"],
-    publication: 1,
-  },
-  {
-    ISBN: "12345Two",
-    title: "Getting started with Python",
-    authors: [1],
-    language: "en",
-    pubDate: "2021-07-07",
-    numOfPage: 225,
-    category: ["fiction", "tech", "web dev"],
-    publication: 1,
-  },
+    {
+        ISBN: "12345ONE",
+        title: "Getting started with MERN",
+        authors: [1, 2],
+        language: "en",
+        pubDate: "2021-07-07",
+        numOfPage: 225,
+        category: ["fiction", "programming", "tech", "web dev"],
+        publication: 1,
+    },
+    {
+        ISBN: "12345Two",
+        title: "Getting started with Python",
+        authors: [1],
+        language: "en",
+        pubDate: "2021-07-07",
+        numOfPage: 225,
+        category: ["fiction", "tech", "web dev"],
+        publication: 1,
+    },
 ];
 
 let Author = [
-  {
-    id: 1,
-    name: "pavan",
-    books: ["12345ONE", "12345Two"],
-  },
-  {
-    id: 2,
-    name: "Deepak",
-    books: ["12345ONE"],
-  },
+    {
+        id: 1,
+        name: "pavan",
+        books: ["12345ONE", "12345Two"],
+    },
+    {
+        id: 2,
+        name: "Deepak",
+        books: ["12345ONE"],
+    },
 ];
 
 let Publication = [
-  {
-    id: 1,
-    name: "Chakra",
-    books: ["12345ONE"],
-  },
-  {
-    id: 2,
-    name: "Vickie Publications",
-    books: [],
-  },
+    {
+        id: 1,
+        name: "Chakra",
+        books: ["12345ONE"],
+    },
+    {
+        id: 2,
+        name: "Vickie Publications",
+        books: [],
+    },
 ];
 
 module.exports = { Book, Author, Publication };
--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const Database = require("./database");
 // initialization
 const OurAPP = express();
 
+OurAPP.use(express.json());
+
 OurAPP.get("/", (request, response) => {
-  response.json({ message: "Server is working!!!!!!" });
+    response.json({ message: "Server is working!!!!!!" });
 });
 
 // Route    - /book
@@ -17,7 +19,7 @@ OurAPP.get("/", (request, response) => {
 // Params   - none
 // Body     - none
 OurAPP.get("/book", (req, res) => {
-  return res.json({ books: Database.Book });
+    return res.json({ books: Database.Book });
 });
 
 // Route    - /book/:bookID
@@ -27,11 +29,11 @@ OurAPP.get("/book", (req, res) => {
 // Params   - bookID
 // Body     - none
 OurAPP.get("/book/:bookID", (req, res) => {
-  const getBook = Database.Book.filter(
-    (book) => book.ISBN === req.params.bookID
-  );
+    const getBook = Database.Book.filter(
+        (book) => book.ISBN === req.params.bookID
+    );
 
-  return res.json({ book: getBook });
+    return res.json({ book: getBook });
 });
 
 // Route    - /book/c/:category
@@ -41,11 +43,11 @@ OurAPP.get("/book/:bookID", (req, res) => {
 // Params   - category
 // Body     - none
 OurAPP.get("/book/c/:category", (req, res) => {
-  const getBook = Database.Book.filter((book) =>
-    book.category.includes(req.params.category)
-  );
+    const getBook = Database.Book.filter((book) =>
+        book.category.includes(req.params.category)
+    );
 
-  return res.json({ book: getBook });
+    return res.json({ book: getBook });
 });
 
 // Route    - /author
@@ -55,7 +57,38 @@ OurAPP.get("/book/c/:category", (req, res) => {
 // Params   - none
 // Body     - none
 OurAPP.get("/author", (req, res) => {
-  return res.json({ author: Database.Author });
+    return res.json({ author: Database.Author });
+});
+
+// Route       /book/new
+// Description add new book
+// Access      PUBLIC
+// Parameters  NONE
+// Method      POST
+OurAPP.post("/book/new", (req, res) => {
+    console.log(req.body);
+    return res.json({ message: "Book Added Successfully" });
+});
+
+// Route     /author/new
+// Description add new author
+// Access PUBLIC
+// Parameters NONE
+// METHOD POST
+OurAPP.post("/author/new", (req, res) => {
+    const { newAuthor } = req.body;
+
+    console.log(newAuthor);
+
+    return res.json({ message: "author was added" });
+});
+
+OurAPP.post("/pulication/new", (req, res) => {
+    const publication = req.body;
+
+    console.log(publication);
+
+    return res.json({ message: "Publication Added" });
 });
 
 OurAPP.listen(4000, () => console.log("Server is running"));
