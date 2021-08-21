416cea69d2e8e2064df25d78ff9582e77d38b0b1
aditya12gusain <adisandi3112002@gmail.com>
2021-08-21 21:11:11 +0530

    Added Mongoose and connected to MongoDB, added .env for storing environment variables

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 // database
 const Database = require("./database");
+
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+    })
+    .then(() => console.log("connection extablished!"))
+    .catch((err) => {
+        console.log(err);
+    });
 
 // initialization
 const OurAPP = express();
